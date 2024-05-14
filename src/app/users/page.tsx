952729ecb1fba911b76c4820c16bc8ce36dd1336
diqimusyaffa29 <diqimusyaffa4@gmail.com>
2024-05-14 23:12:15 +0800

    Done Column Formatting

---
+++ src/app/users/page.tsx
@@ -0,0 +1,30 @@
+import { DataTable } from "@/components/data-table";
+import { User, columns } from "./columns";
+
+export const metadata = {
+    title: "User Page",
+};
+
+
+async function getUsers(): Promise<User[]>{
+    const res = await fetch(
+        'https://66437c0f6c6a656587074681.mockapi.io/users'
+    )
+    const data = await res.json()
+    return data
+}
+
+
+const UserPage = async () => {
+    const data = await getUsers()
+    return (
+        <section className="py-24">
+            <div className="container">
+                <h1 className="text-3xl font-bold">All User</h1>
+                <DataTable columns={columns} data={data} />
+            </div>
+        </section>
+    );
+};
+
+export default UserPage;
